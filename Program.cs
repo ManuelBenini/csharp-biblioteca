2b11f5ff22f4a64723fa65764e630075884e821c
Manuel Benini <manuelbenini1905@gmail.com>
2022-09-20 17:20:07 +0200

    Inserito flusso utente per tornare al menù principale, inserita possibilità di Login

--- Program.cs
+++ Program.cs
@@ -1,6 +1,14 @@
 ﻿
 using System.Linq;
 
+List<User> users = new List<User>
+{
+    new User("Benini", "Manuel", "manuelbenini1905@gmail.com", "ciao19", "3913325333"),
+    new User("John", "Snow", "cicciolino@gmail.com", "beppe19", "3913325333"),
+    new User("artoris", "eleggis", "ercole@gmail.com", "nonloso", "3913325333"),
+    new User("peppe", "Gianluca", "elementare@gmail.com", "wattson", "3913325333"),
+};
+
 List<Book> books = new List<Book>
 { 
     new Book(5534, "Lo Hobbit", 2019, "Fantasy", true, "AB", "J.R.R Tolkien", 540),
@@ -17,7 +25,9 @@ List<Dvd> dvds = new List<Dvd>
     new Dvd(1111, "Jordie", 2019, "Storytelling", false, "ERB", "De Andrè", 110),
 };
 
-Console.WriteLine("Salve! Vuole effettuare una ricerca(0) o registrarsi?(1)");
+Menu:
+
+Console.WriteLine("Salve! Vuole effettuare una ricerca(0) , registrarsi?(1) o effettuare il login(2)");
 int choice = Convert.ToInt32(Console.ReadLine());
 
 if(choice == 0)
@@ -26,18 +36,46 @@ if(choice == 0)
     int productChoice = Convert.ToInt32(Console.ReadLine());
 
     Search(productChoice);
+
+    Console.WriteLine("Tornare al menù? Si(1) No(0)");
+    if (Convert.ToInt32(Console.ReadLine()) == 1)
+    {
+        goto Menu;
+    }
+}
+else if(choice == 1)
+{
+   User user = Registration();
+   users.Add(user);
+
+   Console.WriteLine($"Salve {user.Name} {user.Surname}! Che cosa vuole prenotare? Dvd(0) Libro(1)");
+   int productChoice = Convert.ToInt32(Console.ReadLine());
+
+    UserList(productChoice, user);
+
+    Console.WriteLine("Tornare al menù? Si(1) No(0)");
+    if (Convert.ToInt32(Console.ReadLine()) == 1)
+    {
+        goto Menu;
+    }
 }
 else
 {
-   User user = Registration();
+    User user = Login();
 
-   Console.WriteLine($"Salve {user.Name + user.Surname}! Che cosa vuole prenotare? Libro(0) Dvd(1)");
-   int productChoice = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine($"Salve {user.Name} {user.Surname}! Che cosa vuole prenotare? Dvd(0) Libro(1)");
+    int productChoice = Convert.ToInt32(Console.ReadLine());
 
-    Search(productChoice);
+    UserList(productChoice, user);
+
+    Console.WriteLine("Tornare al menù? Si(1) No(0)");
+    if (Convert.ToInt32(Console.ReadLine()) == 1)
+    {
+        goto Menu;
+    }
 }
 
-void Search(int productChoice)
+Object Search(int productChoice)
 {
     if (productChoice == 0)
     {
@@ -68,10 +106,13 @@ void Search(int productChoice)
             Console.WriteLine($"Scaffale: {chosenDvd.Shelf}");
             Console.WriteLine($"Autore: {chosenDvd.Author}");
             Console.WriteLine($"Numero di pagine: {chosenDvd.Time}");
+
+            return chosenDvd;
         }
         else
         {
             Console.WriteLine("Dvd non trovato");
+            return chosenDvd;
         }
     }
     else
@@ -103,10 +144,13 @@ void Search(int productChoice)
             Console.WriteLine($"Scaffale: {chosenBook.Shelf}");
             Console.WriteLine($"Autore: {chosenBook.Author}");
             Console.WriteLine($"Numero di pagine: {chosenBook.NumberOfPages}");
+
+            return chosenBook;
         }
         else
         {
             Console.WriteLine("Libro non trovato");
+            return chosenBook;
         }
     }
 }
@@ -148,5 +192,115 @@ User Registration()
     } while (userDataAccepted == 0);
 
     return registedUser;
+}
+
+User Login()
+{
+    Console.WriteLine("Inserire la password");
+    string searchedPassword = Console.ReadLine();
+
+    bool userFounded = false;
+    User chosenUser = null;
+
+    foreach (User user in users)
+    {
+        if (user.Password.ToLower().Contains(searchedPassword.ToLower()))
+        {
+            userFounded = true;
+            chosenUser = user;
+        }
+    }
+    if (userFounded)
+    {
+        Console.WriteLine("Utente trovato, ecco i suoi dettagli: ");
+
+        Console.WriteLine($"Cognome: {chosenUser.Surname}");
+        Console.WriteLine($"Nome: {chosenUser.Name}");
+        Console.WriteLine($"Email: {chosenUser.Email}");
+        Console.WriteLine($"Password: {chosenUser.Password}");
+        Console.WriteLine($"Phone: {chosenUser.Phone}");
+
+        return chosenUser;
+    }
+    else
+    {
+        Console.WriteLine("Utente non trovato");
+        return chosenUser;
+    }
+}
+
+void UserList(int productChoice, User user)
+{
+    if (productChoice == 0)
+    {
+        Dvd chosenDvd = (Dvd)Search(productChoice);
+
+        if(chosenDvd != null)
+        {
+            if (chosenDvd.IsAvailable == true)
+            {
+                Console.WriteLine("Il dvd è prenotabile, procedere? Si(1) No(0)");
+                int isUserBooking = Convert.ToInt32(Console.ReadLine());
+
+                if (isUserBooking == 1)
+                {
+                    chosenDvd.IsAvailable = false;
+                    user.DvdPush(chosenDvd);
+
+                    Console.WriteLine($"la prenotazione del Dvd {chosenDvd.Title} è avvenuta con successo! Visualizzare la lista dei propri Dvd? Si(1) No(0)");
+                    int toList = Convert.ToInt32(Console.ReadLine());
+
+                    if (toList == 1)
+                    {
+                        Console.WriteLine("Ecco la lista dei Dvd da lei prenotati: ");
+                        foreach (Dvd dvd in user.getDvds())
+                        {
+                            Console.WriteLine(dvd.Title);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Il Dvd non è prenotabile.");
+            }
+        }
+    }
+    else
+    {
+        Book chosenBook = (Book)Search(productChoice);
+
+        if(chosenBook != null)
+        {
+            if (chosenBook.IsAvailable == true)
+            {
+                Console.WriteLine("Il libro è prenotabile, procedere? Si(1) No(0)");
+                int isUserBooking = Convert.ToInt32(Console.ReadLine());
+
+                if (isUserBooking == 1)
+                {
+                    chosenBook.IsAvailable = false;
+                    user.BookPush(chosenBook);
+
+                    Console.WriteLine($"la prenotazione del libro {chosenBook.Title} è avvenuta con successo! Visualizzare la lista dei propri libri? Si(1) No(0)");
+                    int toList = Convert.ToInt32(Console.ReadLine());
+
+                    if (toList == 1)
+                    {
+                        Console.WriteLine("Ecco la lista dei libri da lei prenotati: ");
+                        foreach (Book book in user.getBooks())
+                        {
+                            Console.WriteLine(book.Title);
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Il libro non è prenotabile.");
+            }
+        }
+        
+    }
 }
 
--- User.cs
+++ User.cs
@@ -6,6 +6,8 @@
     public string Email { get; set; }
     public string Password { get; set; }
     public string Phone { get; set; }
+    public List<Dvd> dvds = new List<Dvd>();
+    public List<Book> books = new List<Book>();
 
     //Constructor
     public User(string surname, string name, string email, string password, string phone)
@@ -20,5 +22,25 @@
     public User()
     {
 
+    }
+
+    public List<Dvd> getDvds()
+    {
+        return dvds;
+    }
+
+    public void DvdPush(Dvd dvd)
+    {
+        dvds.Add(dvd);
+    }
+
+    public List<Book> getBooks()
+    {
+        return books;
+    }
+
+    public void BookPush(Book book)
+    {
+        books.Add(book);
     }
 }
